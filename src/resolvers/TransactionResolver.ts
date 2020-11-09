bcd3a7385730721b6eb94c09fd63ba153af605ac
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-09 09:15:51 -0500

    format resolvers

--- src/resolvers/TransactionResolver.ts
+++ src/resolvers/TransactionResolver.ts
@@ -1,78 +1,77 @@
-import { Resolver, Query, Field, InputType, Mutation, Arg } from "type-graphql";
-import { User } from "../models/User";
-import { Transaction } from "../models/Transaction";
+import { Resolver, Query, Field, InputType, Mutation, Arg } from 'type-graphql'
+import { User } from '../models/User'
+import { Transaction } from '../models/Transaction'
 
 @InputType()
 export class CreateTransaction {
-  @Field()
-  from: string;
+	@Field()
+	from: string
 
-  @Field()
-  to: string;
+	@Field()
+	to: string
 
-  @Field()
-  amount: number = 0;
-  
+	@Field()
+	amount = 0
 }
 
 @Resolver()
 export class TransactionResolver {
-  @Query(() => [Transaction])
-  async transactions() {
-    return await Transaction.find({
-        relations: ["from", "to"]
-    });
-  }
+	@Query(() => [Transaction])
+	async transactions() {
+		return await Transaction.find({
+			relations: ['from', 'to'],
+		})
+	}
 
-  @Query(() => Transaction)
-  async transaction(@Arg("id") id: string) {
-      return await Transaction.findOneOrFail(id, {
-        relations: ["from", "to"]
-    })
-  }
+	@Query(() => Transaction)
+	async transaction(@Arg('id') id: string) {
+		return await Transaction.findOneOrFail(id, {
+			relations: ['from', 'to'],
+		})
+	}
 
-  @Mutation(() => Transaction)
-  async transact(@Arg("data") data: CreateTransaction) {
-    const transaction = new Transaction();
+	@Mutation(() => Transaction)
+	async transact(@Arg('data') data: CreateTransaction) {
+		const transaction = new Transaction()
 
-    transaction.balance=data.amount;
-    transaction.validated=false
+		transaction.balance = data.amount
+		transaction.validated = false
 
-    const fromUser = await User.findOneOrFail(data.from, {
-        relations: ["outgoingTransactions", "incomingTransactions"]
-    })
-    const toUser = await User.findOneOrFail(data.to, {
-        relations: ["outgoingTransactions", "incomingTransactions"]
-    })
-    transaction.from = fromUser
-    transaction.to = toUser
+		const fromUser = await User.findOneOrFail(data.from, {
+			relations: ['outgoingTransactions', 'incomingTransactions'],
+		})
+		const toUser = await User.findOneOrFail(data.to, {
+			relations: ['outgoingTransactions', 'incomingTransactions'],
+		})
+		transaction.from = fromUser
+		transaction.to = toUser
 
-    await transaction.save();
+		await transaction.save()
 
-    fromUser.outgoingTransactions.push(transaction)
-    toUser.incomingTransactions.push(transaction)
-    return transaction;
-  }
+		fromUser.outgoingTransactions.push(transaction)
+		toUser.incomingTransactions.push(transaction)
+		return transaction
+	}
 
-  @Mutation(() => Transaction)
-  async pay(@Arg("id") id: string) {
-    const transaction = await Transaction.findOneOrFail(id, {
-        relations: ["from", "to"]
-    })
+	@Mutation(() => Transaction)
+	async pay(@Arg('id') id: string) {
+		const transaction = await Transaction.findOneOrFail(id, {
+			relations: ['from', 'to'],
+		})
 
-    if(!transaction.validated) {
-        if(transaction.from.balance > transaction.balance) {
-            transaction.from.balance -= transaction.balance
-            transaction.to.balance += transaction.balance
-            await transaction.from.save()
-            await transaction.to.save()
+		if (!transaction.validated) {
+			if (transaction.from.balance > transaction.balance) {
+				transaction.from.balance -= transaction.balance
+				transaction.to.balance += transaction.balance
+				await transaction.from.save()
+				await transaction.to.save()
 
-            transaction.validated = true;
+				transaction.validated = true
 
-            await transaction.save()
-        }
-    }
+				await transaction.save()
+			}
+		}
 
-    return transaction;
-  }
+		return transaction
+	}
 }
--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,52 +1,54 @@
-import { Resolver, Query, Field, InputType, Mutation, Arg } from "type-graphql";
-import { User } from "../models/User";
-import { Transaction } from "../models/Transaction";
-import { getRepository } from "typeorm";
+import { Resolver, Query, Field, InputType, Mutation, Arg } from 'type-graphql'
+import { User } from '../models/User'
+import { Transaction } from '../models/Transaction'
+import { getRepository } from 'typeorm'
 
 @InputType()
 export class CreateUserInput {
-  @Field()
-  id: string;
+	@Field()
+	id: string
 }
 
 @Resolver()
 export class UserResolver {
-  @Query(() => [User])
-  users() {
-    return User.find({
-      relations: [
-        "incomingTransactions",
-        "outgoingTransactions",
-        "outgoingTransactions.to",
-        "outgoingTransactions.from",
-        "incomingTransactions.to",
-        "incomingTransactions.from",
-      ],
-    });
-  }
+	@Query(() => [User])
+	users() {
+		return User.find({
+			relations: [
+				'incomingTransactions',
+				'outgoingTransactions',
+				'outgoingTransactions.to',
+				'outgoingTransactions.from',
+				'incomingTransactions.to',
+				'incomingTransactions.from',
+			],
+		})
+	}
 
-  @Query(() => User)
-  async user(@Arg("id") id: string) {
-    return await User.findOneOrFail(id, {
-      relations: [        "incomingTransactions",
-      "outgoingTransactions",
-      "outgoingTransactions.to",
-      "outgoingTransactions.from",
-      "incomingTransactions.to",
-      "incomingTransactions.from",]
-    })
-  }
+	@Query(() => User)
+	async user(@Arg('id') id: string) {
+		return await User.findOneOrFail(id, {
+			relations: [
+				'incomingTransactions',
+				'outgoingTransactions',
+				'outgoingTransactions.to',
+				'outgoingTransactions.from',
+				'incomingTransactions.to',
+				'incomingTransactions.from',
+			],
+		})
+	}
 
-  @Mutation(() => User)
-  async createUser(@Arg("data") data: CreateUserInput) {
-    const user = User.create(data);
-    user.incomingTransactions = [];
-    user.outgoingTransactions = [];
-    user.balance=0;
-    await user.save();
+	@Mutation(() => User)
+	async createUser(@Arg('data') data: CreateUserInput) {
+		const user = User.create(data)
+		user.incomingTransactions = []
+		user.outgoingTransactions = []
+		user.balance = 0
+		await user.save()
 
-    return await User.findOneOrFail(data.id, {
-      relations: ["incomingTransactions", "outgoingTransactions"],
-    });
-  }
+		return await User.findOneOrFail(data.id, {
+			relations: ['incomingTransactions', 'outgoingTransactions'],
+		})
+	}
 }
