bdba84b5b710a61d23173d598f8e78478c1a11ce
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-09 09:15:48 -0500

    format user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,23 +1,30 @@
-import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, OneToMany, PrimaryColumn } from "typeorm";
-import { ObjectType, Field, ID } from "type-graphql";
-import { Transaction } from "./Transaction";
+import {
+	Entity,
+	BaseEntity,
+	PrimaryGeneratedColumn,
+	Column,
+	OneToMany,
+	PrimaryColumn,
+} from 'typeorm'
+import { ObjectType, Field, ID } from 'type-graphql'
+import { Transaction } from './Transaction'
 
 @Entity()
 @ObjectType()
 export class User extends BaseEntity {
-  @PrimaryColumn()
-  @Field(() => ID)
-  id: string;
+	@PrimaryColumn()
+	@Field(() => ID)
+	id: string
 
-  @Column()
-  @Field(() => Number)
-  balance: number;
-  
-  @Field(() => [Transaction])
-  @OneToMany(() => Transaction, transaction => transaction.from)
-  outgoingTransactions: Transaction[]
+	@Column()
+	@Field(() => Number)
+	balance: number
 
-  @Field(() => [Transaction])
-  @OneToMany(() => Transaction, transaction => transaction.to)
-  incomingTransactions: Transaction[]
+	@Field(() => [Transaction])
+	@OneToMany(() => Transaction, (transaction) => transaction.from)
+	outgoingTransactions: Transaction[]
+
+	@Field(() => [Transaction])
+	@OneToMany(() => Transaction, (transaction) => transaction.to)
+	incomingTransactions: Transaction[]
 }
