b89692ae8fffce78efc53584fd6d447e2f0992b7
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-09 09:15:41 -0500

    format index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import { ApolloServer } from "apollo-server";
-import { buildSchema } from "type-graphql";
-import { UserResolver } from "./resolvers/UserResolver";
-import { TransactionResolver } from "./resolvers/TransactionResolver";
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+import { ApolloServer } from 'apollo-server'
+import { buildSchema } from 'type-graphql'
+import { UserResolver } from './resolvers/UserResolver'
+import { TransactionResolver } from './resolvers/TransactionResolver'
 
-async function main () {
-  await createConnection();
-  const schema = await buildSchema({
-    resolvers: [UserResolver, TransactionResolver],
-  });
-  const server = new ApolloServer({ schema });
-  await server.listen(4000);
-  console.log("Server has started!");
+async function main() {
+	await createConnection()
+	const schema = await buildSchema({
+		resolvers: [UserResolver, TransactionResolver],
+	})
+	const server = new ApolloServer({ schema })
+	await server.listen(4000)
+	console.log('Server has started!')
 }
 
 main()
