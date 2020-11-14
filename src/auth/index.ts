5679ddfc96a618b92b1734777bf7a24523e1f477
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-14 12:11:22 -0500

    auth for everything

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,35 +1,50 @@
 import { AuthChecker } from 'type-graphql'
-import Bot from '../models/Bot'
 import { hashCode } from '../util/index'
+import User from '../models/User'
+import Transaction from '../models/Transaction'
 
-export const customAuthChecker: AuthChecker<any> = async (e: any, a: any) => {
-	// here we can read the user from context
-	// and check his permission in the db against the `roles` argument
-	// that comes from the `@Authorized` decorator, eg. ["ADMIN", "MODERATOR"]
-
+export const authChecker: AuthChecker<any> = async (e: any, a: any) => {
 	let authed = false
 
 	const {
 		context: { headers },
+		info: { fieldName, fieldNodes },
 	} = e
 
-	if (a.includes('admin')) {
-		if (headers.admin) {
-			authed = headers.admin === process.env.ADMIN
-		}
-	} else if (a.includes('bot')) {
-		if (headers.botid) {
-			console.log(headers.botid)
-			if (headers.botsecret) {
-				const bot = await Bot.findOneOrFail(headers.botid)
-				console.log(bot)
-				if (bot) {
-					authed =
-						bot.passwordHash === (await hashCode(headers.botsecret).toString())
-				}
-			}
-		}
-	}
+	if (a.includes('admin') && headers.admin) {
+		authed = headers.admin === process.env.ADMIN
+	} else if (
+		a.includes('authed') &&
+		['send'].includes(fieldName) &&
+		headers.secret
+	) {
+		const fromID = fieldNodes[0].arguments[0].value.fields.filter(
+			(x: any) => x.name.value === 'from'
+		)[0].value.value
+
+		const from = await User.findOneOrFail(fromID)
+
+		console.log(from.secretHash)
+		console.log(hashCode(headers.secret).toString())
+
+		authed = from.secretHash === hashCode(headers.secret).toString()
+	} else if (
+		a.includes('authed') &&
+		['pay'].includes(fieldName) &&
+		headers.secret
+	) {
+		const transactionID = fieldNodes[0].arguments[0].value.value
+
+		const transaction = await Transaction.findOneOrFail(transactionID, {
+			relations: ['from', 'to'],
+		})
+
+		const fromID = transaction.from
+
+		const from = await User.findOneOrFail(fromID)
+
+		authed = from.secretHash === hashCode(headers.secret).toString()
+	} // secret = 3Q9HneBKaDFX5iZ6G9oZQmKtaHLIJsx7
 
 	return authed // or false if access is denied
 }
