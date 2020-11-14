a3cc752940adaf64862a6596037a91622bea6289
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-14 10:39:55 -0500

    remove hash func

--- src/index.ts
+++ src/index.ts
@@ -9,19 +9,6 @@ import { BotResolver } from './resolvers/'
 
 async function main() {
 	await createConnection()
-	Object.defineProperty(String.prototype, 'hashCode', {
-		value: function () {
-			let hash = 0,
-				i,
-				chr
-			for (i = 0; i < this.length; i++) {
-				chr = this.charCodeAt(i)
-				hash = (hash << 5) - hash + chr
-				hash |= 0 // Convert to 32bit integer
-			}
-			return hash
-		},
-	})
 
 	const schema = await buildSchema({
 		resolvers: [UserResolver, TransactionResolver, BotResolver],
