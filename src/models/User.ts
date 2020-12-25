de1587ba58a14cc04dffc48d2d35b4d03e4d42de
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-25 12:20:29 -0500

    add user field resolver

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,15 @@
 import { Entity, BaseEntity, Column, OneToMany, PrimaryColumn } from 'typeorm'
-import { ObjectType, Field, ID } from 'type-graphql'
+import {
+	ObjectType,
+	Field,
+	ID,
+	Resolver,
+	FieldResolver,
+	Root,
+	Arg,
+} from 'type-graphql'
 import Transaction from './Transaction'
+import { PaginationInput } from './Pagination'
 
 @Entity()
 @ObjectType()
@@ -37,4 +46,69 @@ export default class User extends BaseEntity {
 		nullable: true,
 	})
 	secret: string
+}
+
+@Resolver((of) => User)
+export class UserFieldResolver {
+	@FieldResolver(() => [Transaction])
+	async outgoingTransactions(
+		@Root() root: User,
+		@Arg('options', { nullable: true }) options?: PaginationInput
+	) {
+		let x = Transaction.createQueryBuilder('transaction')
+			.where('transaction.fromId = :id', { id: root.id })
+			.innerJoinAndSelect('transaction.from', 'from')
+			.innerJoinAndSelect('transaction.to', 'to')
+
+		if (options?.sort) {
+			x = x.orderBy(
+				`transaction.${options.sort.field}`,
+				options.sort.order || 'ASC'
+			)
+		}
+
+		if (options?.page || options?.skip) {
+			x = x.skip(options.skip || 0 + (options.take || 0) * (options.page || 0))
+		}
+
+		if (options?.take) {
+			x = x.take(options.take)
+		}
+
+		const z = await x.getMany()
+
+		console.log(z)
+		return z
+	}
+
+	@FieldResolver(() => [Transaction])
+	async incomingTransactions(
+		@Root() root: User,
+		@Arg('options', { nullable: true }) options?: PaginationInput
+	) {
+		let x = Transaction.createQueryBuilder('transaction')
+			.where('transaction.toId = :id', { id: root.id })
+			.innerJoinAndSelect('transaction.from', 'from')
+			.innerJoinAndSelect('transaction.to', 'to')
+
+		if (options?.sort) {
+			x = x.orderBy(
+				`transaction.${options.sort.field}`,
+				options.sort.order || 'ASC'
+			)
+		}
+
+		if (options?.page || options?.skip) {
+			x = x.skip(options.skip || 0 + (options.take || 0) * (options.page || 0))
+		}
+
+		if (options?.take) {
+			x = x.take(options.take)
+		}
+
+		const z = await x.getMany()
+
+		console.log(z)
+		return z
+	}
 }
