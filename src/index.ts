f22d880434110005c59ea413f86f8fef2845c040
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-30 21:10:44 -0400

    big imports

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,11 @@ import User, { UserFieldResolver } from './models/User'
 import { WebhookResolver } from './resolvers/'
 import { PaymentWebhook, TransactionWebhook } from './models/Webhook'
 import Transaction from './models/Transaction'
+import Big from 'big.js'
 
 async function main() {
 	await createConnection()
+
 
 	const schema = await buildSchema({
 		resolvers: [
